5f962b2134559ca944d3c1861f915f85f261925e
Avi Wadhwa <aviwad@gmail.com>
2022-06-13 22:55:06 +0530

    Initial Commit

---
+++ Friend Activity for Spotify/Friend_Activity_for_SpotifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Friend_Activity_for_SpotifyApp.swift
+//  Friend Activity for Spotify
+//
+//  Created by Avi Wadhwa on 2022-06-13.
+//
+
+import SwiftUI
+
+@main
+struct Friend_Activity_for_SpotifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
