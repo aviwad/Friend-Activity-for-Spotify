162d7f90f43c5d76b63ed549fcb2e23438ab057b
Avi Wadhwa <aviwad@gmail.com>
2023-05-31 12:09:25 +0530

    1.7 time

--- Friend Activity for Spotify/Views/SettingsPage.swift
+++ Friend Activity for Spotify/Views/SettingsPage.swift
@@ -19,7 +19,7 @@ struct SettingsPage: View {
                             .resizable()
                             .frame(width: 150, height: 150)
                             .cornerRadius(10)
-                        Text("Version 1.6")
+                        Text("Version 1.7")
                             .font(.custom("montserrat", size: 20))
                         Text("App made by Avi Wadhwa")
                             .font(.custom("montserrat", size: 15))
